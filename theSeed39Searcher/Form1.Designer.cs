fec20519672d9492d4f4d9f68ebb83b0b9976398
Min <mijien0179@gmail.com>
2022-08-02 19:52:16 +0900

    초기 커밋.

---
+++ theSeed39Searcher/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace theSeed39Searcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.설정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.정답색ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.굵게표시ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.포인트추가ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.창활성화시질문삭제ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.창설정ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.맨위로ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.투명도80ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.개발자GithubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.데이터출처ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("맑은 고딕", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(823, 82);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(823, 99);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 370);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 32);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.설정ToolStripMenuItem,
+            this.창설정ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(847, 33);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 설정ToolStripMenuItem
+            // 
+            this.설정ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.정답색ToolStripMenuItem,
+            this.굵게표시ToolStripMenuItem,
+            this.포인트추가ToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.창활성화시질문삭제ToolStripMenuItem});
+            this.설정ToolStripMenuItem.Name = "설정ToolStripMenuItem";
+            this.설정ToolStripMenuItem.Size = new System.Drawing.Size(106, 29);
+            this.설정ToolStripMenuItem.Text = "정답 설정";
+            // 
+            // 정답색ToolStripMenuItem
+            // 
+            this.정답색ToolStripMenuItem.Name = "정답색ToolStripMenuItem";
+            this.정답색ToolStripMenuItem.Size = new System.Drawing.Size(300, 34);
+            this.정답색ToolStripMenuItem.Text = "글자 색상";
+            this.정답색ToolStripMenuItem.Click += new System.EventHandler(this.정답색ToolStripMenuItem_Click);
+            // 
+            // 굵게표시ToolStripMenuItem
+            // 
+            this.굵게표시ToolStripMenuItem.Checked = true;
+            this.굵게표시ToolStripMenuItem.CheckOnClick = true;
+            this.굵게표시ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.굵게표시ToolStripMenuItem.Name = "굵게표시ToolStripMenuItem";
+            this.굵게표시ToolStripMenuItem.Size = new System.Drawing.Size(300, 34);
+            this.굵게표시ToolStripMenuItem.Text = "굵게 표시";
+            // 
+            // 포인트추가ToolStripMenuItem
+            // 
+            this.포인트추가ToolStripMenuItem.Checked = true;
+            this.포인트추가ToolStripMenuItem.CheckOnClick = true;
+            this.포인트추가ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.포인트추가ToolStripMenuItem.Name = "포인트추가ToolStripMenuItem";
+            this.포인트추가ToolStripMenuItem.Size = new System.Drawing.Size(300, 34);
+            this.포인트추가ToolStripMenuItem.Text = "포인트 추가";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(297, 6);
+            // 
+            // 창활성화시질문삭제ToolStripMenuItem
+            // 
+            this.창활성화시질문삭제ToolStripMenuItem.Checked = true;
+            this.창활성화시질문삭제ToolStripMenuItem.CheckOnClick = true;
+            this.창활성화시질문삭제ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.창활성화시질문삭제ToolStripMenuItem.Name = "창활성화시질문삭제ToolStripMenuItem";
+            this.창활성화시질문삭제ToolStripMenuItem.Size = new System.Drawing.Size(300, 34);
+            this.창활성화시질문삭제ToolStripMenuItem.Text = "창 활성화 시 질문 삭제";
+            // 
+            // 창설정ToolStripMenuItem
+            // 
+            this.창설정ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.맨위로ToolStripMenuItem,
+            this.투명도80ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.개발자GithubToolStripMenuItem,
+            this.데이터출처ToolStripMenuItem});
+            this.창설정ToolStripMenuItem.Name = "창설정ToolStripMenuItem";
+            this.창설정ToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.창설정ToolStripMenuItem.Text = "창 설정";
+            // 
+            // 맨위로ToolStripMenuItem
+            // 
+            this.맨위로ToolStripMenuItem.Checked = true;
+            this.맨위로ToolStripMenuItem.CheckOnClick = true;
+            this.맨위로ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.맨위로ToolStripMenuItem.Name = "맨위로ToolStripMenuItem";
+            this.맨위로ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.맨위로ToolStripMenuItem.Text = "맨위로";
+            this.맨위로ToolStripMenuItem.Click += new System.EventHandler(this.맨위로ToolStripMenuItem_Click);
+            // 
+            // 투명도80ToolStripMenuItem
+            // 
+            this.투명도80ToolStripMenuItem.CheckOnClick = true;
+            this.투명도80ToolStripMenuItem.Name = "투명도80ToolStripMenuItem";
+            this.투명도80ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.투명도80ToolStripMenuItem.Text = "투명도 80%";
+            this.투명도80ToolStripMenuItem.Click += new System.EventHandler(this.투명도80ToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(267, 6);
+            // 
+            // 개발자GithubToolStripMenuItem
+            // 
+            this.개발자GithubToolStripMenuItem.Name = "개발자GithubToolStripMenuItem";
+            this.개발자GithubToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.개발자GithubToolStripMenuItem.Text = "개발자 github";
+            this.개발자GithubToolStripMenuItem.Click += new System.EventHandler(this.개발자GithubToolStripMenuItem_Click);
+            // 
+            // 데이터출처ToolStripMenuItem
+            // 
+            this.데이터출처ToolStripMenuItem.Name = "데이터출처ToolStripMenuItem";
+            this.데이터출처ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.데이터출처ToolStripMenuItem.Text = "데이터 출처";
+            this.데이터출처ToolStripMenuItem.Click += new System.EventHandler(this.데이터출처ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 430);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Maple TheSeed 39 Floor Searcher";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem 설정ToolStripMenuItem;
+        private ToolStripMenuItem 정답색ToolStripMenuItem;
+        private ToolStripMenuItem 굵게표시ToolStripMenuItem;
+        private ToolStripMenuItem 포인트추가ToolStripMenuItem;
+        private ToolStripMenuItem 창설정ToolStripMenuItem;
+        private ToolStripMenuItem 맨위로ToolStripMenuItem;
+        private ToolStripMenuItem 투명도80ToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem 개발자GithubToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem 창활성화시질문삭제ToolStripMenuItem;
+        private ToolStripMenuItem 데이터출처ToolStripMenuItem;
+    }
+}
---
+++ theSeed39Searcher/Form1.cs
@@ -0,0 +1,137 @@
+using System.Diagnostics;
+
+namespace theSeed39Searcher
+{
+    public partial class Form1 : Form
+    {
+        String UpdateDate = "2022.08.02.";
+        Color AnswerColor = Color.Red;
+        public Form1()
+        {
+            InitializeComponent();
+            string qString = Properties.Resources.file;
+
+            맨위로ToolStripMenuItem_Click(맨위로ToolStripMenuItem, new EventArgs());
+            투명도80ToolStripMenuItem_Click(투명도80ToolStripMenuItem, new EventArgs());
+            textBox1_TextChanged(textBox1, new EventArgs());
+
+            searchList = qString.Split("\n");
+            ImeMode = ImeMode.Hangul;
+        }
+
+        string[] searchList;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text += $" {UpdateDate}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var answer = new Label[] { label2, label3, label4, label5 };
+            foreach (var item in answer)
+            {
+                item.Text = "";
+            }
+            if (textBox1.Text == "")
+            {
+                label1.Text = "질문을 입력하세요.";
+                return;
+            }
+            var data = textBox1.Text.Split(" ");
+
+            int b = 0;
+
+            string selected = "답이 없습니다.";
+            foreach (var str in searchList)
+            {
+                b = 0;
+                foreach (var item in data)
+                {
+                    if (!str.Contains(item))
+                    {
+                        break;
+                    }
+                    b++;
+                }
+                if (b == data.Length)
+                {
+                    selected = str;
+                    break;
+                }
+            }
+
+
+            var items = selected.Split("|");
+            for (int i = 0; i < items.Length; i++) items[i] = items[i].Trim();
+
+            label1.Text = items[0];
+
+            if (b != data.Length) return;
+
+            for (int i = 0; i < 4; ++i)
+            {
+                FontStyle style = FontStyle.Regular;
+                answer[i].Text = $"{i + 1}. {items[i + 1]}";
+                answer[i].ForeColor = Color.Black;
+                if (i + 1 == items[5][0] - '0')
+                {
+                    answer[i].ForeColor = AnswerColor;
+                    if (굵게표시ToolStripMenuItem.Checked == true)
+                    {
+                        style = FontStyle.Bold;
+                    }
+
+                    if (포인트추가ToolStripMenuItem.Checked == true)
+                    {
+                        answer[i].Text += " *";
+                    }
+                }
+                answer[i].Font = new Font("돋움체", 11, style);
+            }
+
+
+        }
+
+        private void 맨위로ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopMost = 맨위로ToolStripMenuItem.Checked;
+        }
+
+        private void 투명도80ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (투명도80ToolStripMenuItem.Checked == true)
+            {
+                this.Opacity = 0.8;
+            }
+            else
+                this.Opacity = 1.0;
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            textBox1.Select();
+            if (창활성화시질문삭제ToolStripMenuItem.Checked) textBox1.Clear();
+        }
+
+        private void 개발자GithubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer", "https://github.com/mijien0179/theSeed39Searcher");
+
+        }
+
+        private void 정답색ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK){
+                AnswerColor = cd.Color;
+            }
+
+        }
+
+        private void 데이터출처ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer", "http://39.theseed.ze.am/");
+        }
+    }
+}
